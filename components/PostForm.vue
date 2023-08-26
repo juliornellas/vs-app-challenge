b6bbd49d3b1625db4ec614fdfa831c4c4620b2d0
Julio <juliornellas@gmail.com>
2023-08-26 15:23:53 -0300

    Add the ability for users to create posts

---
+++ components/PostForm.vue
@@ -0,0 +1,24 @@
+<script setup>
+function submit() {}
+</script>
+
+<template>
+  <form class="grid gap-4 mb-16" @submit.prevent="submit">
+    <input
+      v-model="post.title"
+      placeholder="Post title"
+      class="block w-full rounded-lg border border-gray-400 px-5 py-4 text-sm focus:border-blue-500 focus:outline-none md:text-base"
+    />
+    <textarea
+      v-model="post.body"
+      placeholder="What is happening?!"
+      class="block w-full rounded-lg border border-gray-400 px-5 py-4 text-sm focus:border-blue-500 focus:outline-none md:text-base"
+    ></textarea>
+    <button
+      class="bg-blue-600 text-white px-8 py-4 rounded-lg"
+      @click.prevent="submit"
+    >
+      Post
+    </button>
+  </form>
+</template>
